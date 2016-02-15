ba53d54f25fbbdfa6a3cd9d541906464b79d9a1e
luksvt <luksvt25@gmail.com>
2016-02-15 22:17:48 +0100

    l.v.process

--- example_scripts/result_analysis.py
+++ example_scripts/result_analysis.py
@@ -8,87 +8,12 @@ import sys
 import shutil
 from Bio import SeqIO
 
-'''The function simple_example_search was stolen from one of the example scripts of ursgal and was only used to test the function of
-the engines. 
+'''
 The function Cre_PCGSD acts on a high resolution dataset and was thought to be the target of the last function comparative_analysis. 
 The latter should compare the outputs of database and denovo engines. '''
 
-def simple_example_search():
-    '''
-    Executes a search with OMSSA, XTandem, MS-GF+ and MS Amanda on the BSA1.mzML
-    input_file
-
-    usage:
-        ./simple_example_search.py
-
-    Note:
-        myrimatch does not work with this file in this case
-
-    '''
-    uc = ursgal.UController(
-        profile = 'LTQ XL low res',
-        params = {
-            'database' : os.path.join( os.pardir, 'example_data', 'BSA.fasta' ),
-            'modifications' : [
-                'M,opt,any,Oxidation',        # Met oxidation
-                'C,fix,any,Carbamidomethyl',  # Carbamidomethylation
-                '*,opt,Prot-N-term,Acetyl'    # N-Acteylation
-            ],
-            'scan_skip_modulo_step' :None
-        }
-    )
-
-    if sys.platform == 'win32':
-        msamanda = 'msamanda_1_0_0_5242'
-    else: 
-        msamanda = 'msamanda_1_0_0_5243'
-
-    engine_list = [
-        'omssa',
-        'xtandem_piledriver',
-        'msgf',
-         #msamanda,
-        'novor',
-        'pepnovo',
-        #'uninovo',
-    ]
-    
-    mzML_file = os.path.join(
-        os.pardir,
-        'example_data',
-        'BSA_simple_example_search',
-        'BSA1.mzML'
-    )
-    if os.path.exists(mzML_file) is False:
-        uc.params['http_url'] = 'http://sourceforge.net/p/open-ms/code/HEAD/tree/OpenMS/share/OpenMS/examples/BSA/BSA1.mzML?format=raw'
-        uc.params['http_output_folder'] = os.path.dirname(mzML_file)
-        uc.fetch_file(
-            engine     = 'get_http_files_1_0_0'
-        )
-        shutil.move(
-            '{0}format=raw'.format(mzML_file),
-            mzML_file
-        )
-
-    unified_file_list = []
-
-    for engine in engine_list:
-        unified_search_result_file = uc.search(
-            input_file = mzML_file,
-            engine     = engine,
-            force      = True
-        )
-        unified_file_list.append(unified_search_result_file)
-
-    uc.visualize(
-        input_files    = unified_file_list,
-        engine         = 'venndiagram',
-        force          = True,
-    )
-    return
-
 def Cre_PCGSD ():
-    database_path = 'C:/Cre_PCGSD/Creinhardtii_281_v5_5_CP_MT_with_contaminants_target_decoy.fasta'
+    database_path = 'C:/Cre_PCGSD/Creinhardtii_281_v5_5_CP_MT_with_contaminants_target_decoy.fasta' #path to fasta database
     uc = ursgal.UController(
         profile = 'QExactive+',
         params = {
@@ -100,7 +25,7 @@ def Cre_PCGSD ():
             #],
         }
     )
-    uc.scan_rt_lookup_path = 'C:/Cre_PCGSD/_ursgal_lookup.pkl'
+    uc.scan_rt_lookup_path = 'C:/Cre_PCGSD/_ursgal_lookup.pkl' #path to pkl files
 
     if sys.platform == 'win32':
         msamanda = 'msamanda_1_0_0_5242'
@@ -116,7 +41,7 @@ def Cre_PCGSD ():
         'pepnovo',
         #'uninovo',
     ]
-    mgf_path = 'C:/Cre_PCGSD/'
+    mgf_path = 'C:/Cre_PCGSD/' #path to mgf files
     mgf_files = []
     for elem in os.listdir(mgf_path):
         if elem.endswith('.mgf'):
@@ -294,8 +219,7 @@ def comparative_analysis( path_to_mgf, database_path ):
     return
 
 def main():
-    simple_example_search()
-    #Cre_PCGSD()
+    Cre_PCGSD()
     #mgf_path = ''
     #database_path = ''
     #comparative_analysis(mgf_path, database_path)
