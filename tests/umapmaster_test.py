56235e8783358e18447cfbe6ba8dd1833cf44718
Christian Fufezan <christian@fufezan.net>
2016-03-01 16:20:43 +0100

    First fkt

--- tests/umapmaster_test.py
+++ tests/umapmaster_test.py
@@ -7,9 +7,49 @@ import pprint
 from ursgal import umapmaster as umama
 
 
+TEST_DICT = {
+    'enzyme' : {
+        'default' : 'Try',
+        'available_in_unode' : ['omssa', 'xtandem'],
+        'description' : 'Enzyme used for digestions',
+        'ukey-translation' : {
+            'omssa_style_1' : '-ot',
+            'xtandem_style_1' : 'enzyme'
+        },
+        'uvalue-translation' : {
+            'try' : {
+                'omssa_style_1': 1,
+                'xtandem_style_1' : 'try'
+            }
+        }
+    },
+    'frag_mass_type' : {
+        'default' : 'monoisotopic',
+        'available_in_unode' : ['omssa'],
+        'description' : 'use chemical average or monoisotopic mass for fragment ions.',
+        'ukey-translation' : {
+            'omssa_style_1' : '-tom',
+        },
+        'uvalue-translation' : {
+            'monoisotopic' : {
+                'omssa_style_1': 0,
+            },
+            'average' : {
+                'omssa_style_1': 1,
+            },
+        }
+    },
+}
+
+
 class UMapMaster(unittest.TestCase):
     def setUp(self):
-        self.umama = umama.UParam.Mapper()
+        self.umama = umama.UParamMapper( TEST_DICT )
+
+
+    def test_get_omssa_all_params(self):
+        all_omssa_params = self.umama.get_all_params( engine='omssa')
+        self.assertEqual(all_omssa_params, ['enzyme', 'frag_mass_type'])
 
 
 if __name__ == '__main__':
--- ursgal/umapmaster.py
+++ ursgal/umapmaster.py
@@ -9,14 +9,23 @@
 """
 
 
-class UParamMapper( object ):
+class UParamMapper( dict ):
     '''
     '''
     def __init__( self, params_dict ):
         self._parse_params( params_dict )
 
     def _parse_params( self, params_dict ):
-        pass
+        self.clear()
+        self.update( params_dict )
+
+    def get_all_params( self, engine=None):
+        assert engine is not None, 'must define engine!'
+        all_params = []
+        for ukey in self.keys():
+            if engine in self[ ukey ]['available_in_unode']:
+                all_params.append( ukey )
+        return all_params
 
 if __name__ == '__main__':
     print('Yes!')
