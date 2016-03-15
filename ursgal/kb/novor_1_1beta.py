30dfa5806d62282496e0c1f05136c70ec59184f0
stefan <s_sch016@uni-muenster.de>
2016-03-15 08:48:54 +0100

    novor kb

--- ursgal/kb/novor_1_1beta.py
+++ ursgal/kb/novor_1_1beta.py
@@ -28,49 +28,49 @@
 #     'include_in_git'            : False,
 # }
 
-DEFAULT_PARAMS = {
-    'novor_forbidden_residues'    : 'I,U',
-    'validation_score_field'      : 'Novor:score',
+# DEFAULT_PARAMS = {
+#     'novor_forbidden_residues'    : 'I,U',
+#     'validation_score_field'      : 'Novor:score',
 
-}
+# }
 
-USEARCH_PARAM_VALUE_TRANSLATIONS = {
-    'trypsin'                : 'Trypsin',
-    'cid'                    : 'CID',
-    'hcd'                    : 'HCD',
-    'da'                     : 'Da',
-    'low_res_LTQ'            : 'Trap',
-    'high_res_LTQ'           : 'Trap',
-    'tof'                    : 'TOF',
-    'q_exactive'             : 'FT',
+# USEARCH_PARAM_VALUE_TRANSLATIONS = {
+#     'trypsin'                : 'Trypsin',
+#     'cid'                    : 'CID',
+#     'hcd'                    : 'HCD',
+#     'da'                     : 'Da',
+#     'low_res_LTQ'            : 'Trap',
+#     'high_res_LTQ'           : 'Trap',
+#     'tof'                    : 'TOF',
+#     'q_exactive'             : 'FT',
 
-    ' scanNum'               : 'Spectrum ID',
-    ' peptide'               : 'Sequence',
-    ' score'                 : 'Novor:score',
-    ' mz(data)'              : 'Exp m/z',
-    ' RT'                    : 'Retention Time (s)',
-    ' z'                     : 'Charge',
-    '# id'                   : 'Novor:id',
-    ' pepMass(denovo)'       : 'Calc mass',
-    ' err(data-denovo)'      : 'Error (exp-calc)',
-    ' ppm(1e6*err/(mz*z))'   : 'Error (ppm)',
-    ' aaScore'               : 'Novor:aaScore',
+#     ' scanNum'               : 'Spectrum ID',
+#     ' peptide'               : 'Sequence',
+#     ' score'                 : 'Novor:score',
+#     ' mz(data)'              : 'Exp m/z',
+#     ' RT'                    : 'Retention Time (s)',
+#     ' z'                     : 'Charge',
+#     '# id'                   : 'Novor:id',
+#     ' pepMass(denovo)'       : 'Calc mass',
+#     ' err(data-denovo)'      : 'Error (exp-calc)',
+#     ' ppm(1e6*err/(mz*z))'   : 'Error (ppm)',
+#     ' aaScore'               : 'Novor:aaScore',
 
-}
+# }
 
-USEARCH_PARAM_KEY_VALUE_TRANSLATOR = {
-}
+# USEARCH_PARAM_KEY_VALUE_TRANSLATOR = {
+# }
 
-USED_USEARCH_PARAMS = set( [
-    'modifications',
-    'enzyme',
-    'precursor_mass_tolerance_unit',
-    'precursor_mass_tolerance_minus',
-    'precursor_mass_tolerance_plus',
-    'frag_mass_tolerance',
-    'frag_mass_tolerance_unit',
-    'frag_method',
-    'instrument'
-    ]
-     )
+# USED_USEARCH_PARAMS = set( [
+#     'modifications',
+#     'enzyme',
+#     'precursor_mass_tolerance_unit',
+#     'precursor_mass_tolerance_minus',
+#     'precursor_mass_tolerance_plus',
+#     'frag_mass_tolerance',
+#     'frag_mass_tolerance_unit',
+#     'frag_method',
+#     'instrument'
+#     ]
+#      )
 
