307db8fa2b306c19575e62f6c75d82d91e7eb4a4
Jun Yang <David@huntsman-ve502-0607.apn.wlan.upenn.edu>
2018-07-02 16:00:15 -0400

    debug unify_csv precursor_mz look up

--- ursgal/resources/platform_independent/arc_independent/unify_csv_1_0_0/unify_csv_1_0_0.py
+++ ursgal/resources/platform_independent/arc_independent/unify_csv_1_0_0/unify_csv_1_0_0.py
@@ -486,15 +486,14 @@ def main(input_file=None, output_file=None, scan_rt_lookup=None,
                 rt_corr_factor = 60
             line_dict['Retention Time (s)'] = float( retention_time_in_minutes ) * rt_corr_factor
 
-            precursor_mz = scan_rt_lookup[ input_file_basename_for_rt_lookup ][
-                'scan_2_mz' ][ spectrum_id ]
-
             print('input_file_basename')    
             print(scan_rt_lookup.keys())
             print('scan_2_mz')
             print(scan_rt_lookup[input_file_basename_for_rt_lookup]['scan_2_mz'].keys())
-            
 
+            precursor_mz = scan_rt_lookup[ input_file_basename_for_rt_lookup ][
+                'scan_2_mz' ][ spectrum_id ]
+            
             line_dict['Exp m/z'] = round(precursor_mz, 10)
 
             #########################
