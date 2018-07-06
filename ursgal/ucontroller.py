76158023078b2efe6fc4af99d494323eb090c572
Jun Yang <David@huntsman-ve502-0516.apn.wlan.upenn.edu>
2018-07-06 17:27:59 -0400

    debug ucontroller

--- ursgal/ucontroller.py
+++ ursgal/ucontroller.py
@@ -1307,7 +1307,7 @@ class UController(ursgal.UNode):
             # for json dump.. evals are always done on loaded
             # i_finfo and o_finfo
         print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
-        print(self.io['input']['finfo']['json_exists'])
+        print(self.io['input']['finfo'].keys())
         print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
 
         if self.io['input']['finfo']['json_exists']:
