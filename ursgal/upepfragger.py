d9787963af5e3a63d6d86c196734216d53a1bba1
Christian Fufezan <christian.x.fufezan@gsk.com>
2019-08-23 13:46:23 +0200

    Added more strings to the df

--- ursgal/upepfragger.py
+++ ursgal/upepfragger.py
@@ -57,21 +57,67 @@ class UPeptideFragmentor:
             self.mods = split[1].split(';')
 
         self.fragment_starts_forward = {
-                'a': {'cc': {'C': -1, 'O': -1}, 'name_format_string' : 'a{pos}'},
-                'b': {'cc': {}, 'name_format_string' : 'b{pos}'},
-                'c': {'cc': {'N': +1, 'H': +3}, 'name_format_string' : 'c{pos}'},
-                # 'c(-1)': {'cc': {'N': +1, 'H': +2}, 'name_format_string' : 'c)-1){pos}'},
-                # 'c(+1)': {'cc': {'N': +1, 'H': +4}, 'name_format_string' : 'c)+1){pos}'},
-                # 'c(+2)': {'cc': {'N': +1, 'H': +5}, 'name_format_string' : 'c)+2){pos}'},
+                'a': {
+                    'cc': {'C': -1, 'O': -1},
+                    'name_format_string' : 'a{pos}',
+                    'html_format_string' : 'a<sub>{pos}</sub>{modstring}<sup>{charge}+</sup>'
+                },
+                'b': {
+                    'cc': {},
+                    'name_format_string' : 'b{pos}',
+                    'html_format_string' : 'a<sub>{pos}</sub>{modstring}<sup>{charge}+</sup>'
+                },
+                'c': {
+                    'cc': {'N': +1, 'H': +3},
+                    'name_format_string' : 'c{pos}',
+                    'html_format_string' : 'a<sub>{pos}</sub>{modstring}<sup>{charge}+</sup>'
+                },
+                # 'c(-1)': {
+                #     'cc': {'N': +1, 'H': +2},
+                #     'name_format_string' : 'c(-1){pos}'
+                # },
+                # 'c(+1)': {
+                #     'cc': {'N': +1, 'H': +4},
+                #     'name_format_string' : 'c(+1){pos}'
+                # },
+                # 'c(+2)': {
+                #     'cc': {'N': +1, 'H': +5},
+                #     'name_format_string' : 'c(+2){pos}'
+                # },
         }
         self.fragment_starts_reverse = {
-                'x': {'cc': {'O': 2, 'C': 1}, 'name_format_string' : 'x{pos}'},
-                'y': {'cc': {'H': 2, 'O': 1}, 'name_format_string' : 'y{pos}'},
-                'Y': {'cc': {'H': 0, 'O': 1}, 'name_format_string' : 'Y{pos}'},
-                'z': {'cc': {'O': 1, 'N': -1, 'H': 0}, 'name_format_string' : 'z{pos}'},
-                # 'z(+1)': {'cc': {'O': 1, 'N': -1, 'H': 1}, 'name_format_string' : 'z(+1){pos}'},
-                # 'z(+2)': {'cc': {'O': 1, 'N': -1, 'H': 2}, 'name_format_string' : 'z(+2){pos}'},
-                # 'z(+3)': {'cc': {'O': 1, 'N': -1, 'H': 3}, 'name_format_string' : 'z(+3){pos}'},
+                'x': {
+                    'cc': {'O': 2, 'C': 1},
+                    'name_format_string' : 'x{pos}',
+                    'html_format_string' : 'x<sub>{pos}</sub>{modstring}<sup>{charge}+</sup>'
+                },
+                'y': {
+                    'cc': {'H': 2, 'O': 1},
+                    'name_format_string' : 'y{pos}',
+                    'html_format_string' : 'y<sub>{pos}</sub>{modstring}<sup>{charge}+</sup>'
+                },
+                'Y': {
+                    'cc': {'H': 0, 'O': 1},
+                    'name_format_string' : 'Y{pos}',
+                    'html_format_string' : 'Y<sub>{pos}</sub>{modstring}<sup>{charge}+</sup>'
+                },
+                'z': {
+                    'cc': {'O': 1, 'N': -1, 'H': 0},
+                    'name_format_string' : 'z{pos}',
+                    'html_format_string' : 'z<sub>{pos}</sub>{modstring}<sup>{charge}+</sup>'
+                },
+                # 'z(+1)': {
+                #     'cc': {'O': 1, 'N': -1, 'H': 1},
+                #     'name_format_string' : 'z(+1){pos}'
+                # },
+                # 'z(+2)': {
+                #     'cc': {'O': 1, 'N': -1, 'H': 2},
+                #     'name_format_string' : 'z(+2){pos}'
+                # },
+                # 'z(+3)': {
+                #     'cc': {'O': 1, 'N': -1, 'H': 3},
+                #     'name_format_string' : 'z(+3){pos}'
+                # },
         }
         abc_ions = self._fragfest(
             forward=True,
@@ -96,11 +142,14 @@ class UPeptideFragmentor:
                         start_dict={
                             'I(b)' : {
                                 'cc': {},
-                                'name_format_string': 'Internal({seq})'
+                                'name_format_string': 'Internal({seq})',
+                                'html_format_string': 'Internal({seq})'
+
                             },
                             'I(a)' : {
                                 'cc': {'C': -1, 'O': -1},
-                                'name_format_string': 'I-28({seq})'
+                                'name_format_string': 'I-28({seq})',
+                                'html_format_string': 'I-28({seq})'
                             }
 
                         },
@@ -115,7 +164,8 @@ class UPeptideFragmentor:
                     all_rows += pos_dict[pos][ion_type]
 
         # self.df = self._induce_fragmentation_of_ion_ladder()
-        self.df = pd.DataFrame(all_rows)
+        self.df = pd.DataFrame(all_rows).sort_values(by='mz').reset_index()
+        self.df.drop(columns=['html_format_string', 'name_format_string'], inplace=True)
 
     def _init_pos0(self, start_dict):
         r = {'pos0' : {}}
@@ -126,6 +176,7 @@ class UPeptideFragmentor:
                     'cc': ChemicalComposition(),
                     'mods': [],
                     'name_format_string': start_dict[ion_type]['name_format_string'],
+                    'html_format_string': start_dict[ion_type]['html_format_string'],
                     'seq': ''
                 }]
             r['pos0'][ion_type][0]['cc'] += start_dict[ion_type]['cc']
@@ -186,28 +237,29 @@ class UPeptideFragmentor:
                     if nl_limited_to_specific_ion_series:
                         if ion_type not in available_in_series:
                             continue
-
-                    for ion_frag in ion_fragments:
-                        new_ion_frag = copy.deepcopy(ion_frag)
-                        new_ion_frag['pos'] += 1
-                        new_ion_frag['cc'] += cc
-                        new_ion_frag['cc'] += neutral_loss_dict.get('cc', {})
-                        mod = neutral_loss_dict.get('name', None)
-                        if mod is not None:
-                            new_ion_frag['mods'].append(mod)
-                        new_ion_frag['hill'] = new_ion_frag['cc'].hill_notation_unimod()
-                        new_ion_frag['charge'] = 1
-                        new_ion_frag['predicted intensity'] = np.NAN
-                        new_ion_frag['mass'] = new_ion_frag['cc']._mass()
-                        new_ion_frag['mz'] = new_ion_frag['mass'] + ursgal.ukb.PROTON
-                        new_ion_frag['series'] = ion_type
-                        new_ion_frag['modstring'] = ','.join(sorted(new_ion_frag['mods']))
-                        new_ion_frag['seq'] += aa
-                        new_ion_frag['name'] = new_ion_frag['name_format_string'].format(**new_ion_frag)
-                        _id = '{name}{modstring}'.format(**new_ion_frag)
-                        if _id not in alread_seen_frags:
-                            pos_dict['pos{0}'.format(dpos+1)][ion_type].append(new_ion_frag)
-                        alread_seen_frags.add(_id)
+                    for charge in self.charges:
+                        for ion_frag in ion_fragments:
+                            new_ion_frag = copy.deepcopy(ion_frag)
+                            new_ion_frag['pos'] += 1
+                            new_ion_frag['cc'] += cc
+                            new_ion_frag['cc'] += neutral_loss_dict.get('cc', {})
+                            mod = neutral_loss_dict.get('name', None)
+                            if mod is not None:
+                                new_ion_frag['mods'].append(mod)
+                            new_ion_frag['hill'] = new_ion_frag['cc'].hill_notation_unimod()
+                            new_ion_frag['charge'] = charge
+                            new_ion_frag['predicted_intensity'] = np.NAN
+                            new_ion_frag['mass'] = new_ion_frag['cc']._mass()
+                            new_ion_frag['mz'] = new_ion_frag['mass'] / charge + ursgal.ukb.PROTON
+                            new_ion_frag['series'] = ion_type
+                            new_ion_frag['modstring'] = ','.join(sorted(new_ion_frag['mods']))
+                            new_ion_frag['seq'] += aa
+                            new_ion_frag['name'] = new_ion_frag['name_format_string'].format(**new_ion_frag)
+                            new_ion_frag['html_name'] = new_ion_frag['html_format_string'].format(**new_ion_frag)
+                            _id = '{name}{modstring}{charge}'.format(**new_ion_frag)
+                            if _id not in alread_seen_frags:
+                                pos_dict['pos{0}'.format(dpos+1)][ion_type].append(new_ion_frag)
+                            alread_seen_frags.add(_id)
         if delete_pos0:
             del pos_dict['pos0']
 
