0233c9c144e98b8f13f24d0e97cf529215a9acb1
StSchulze <schulzestefansts@gmail.com>
2019-10-29 14:25:58 -0400

    fixing setup.py by

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ class InstallResourcesCommand(distutils.cmd.Command):
 
         '''
         command = [sys.executable]
-        if 'ursgal' in os.path.basename(os.getcwd()):
+        if 'ursgal' in os.path.basename(os.path.realpath(__file__)):
             ursgal_directory = os.getcwd()
         else:
             print('Could not find ursgal directory')
