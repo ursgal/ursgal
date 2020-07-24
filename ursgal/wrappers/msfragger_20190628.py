a4a2fc588437f50720417653d3edc740f2fb273e
StSchulze <schulzestefansts@gmail.com>
2020-07-24 17:07:40 -0400

    rename msfragger params file

--- ursgal/wrappers/msfragger_20190628.py
+++ ursgal/wrappers/msfragger_20190628.py
@@ -88,6 +88,11 @@ class msfragger_20190628(ursgal.UNode):
         Returns:
                 dict: self.params
         '''
+        self.input_file = os.path.join(
+            self.params['input_dir_path'],
+            self.params['input_file']
+        )
+        
         self.param_file_name = os.path.join(
             self.params['output_dir_path'],
             '{0}_msfragger.params'.format(self.input_file)
@@ -262,12 +267,9 @@ class msfragger_20190628(ursgal.UNode):
 
                 else:
                     self.params_to_write[msfragger_param_name] = param_value
-        self.write_params_file()
 
-        self.input_file = os.path.join(
-            self.params['input_dir_path'],
-            self.params['input_file']
-        )
+        self.write_params_file()
+        
         if self.input_file.lower().endswith('.mzml') or \
                 self.input_file.lower().endswith('.mzml.gz') or \
                 self.input_file.lower().endswith('.mgf'):
